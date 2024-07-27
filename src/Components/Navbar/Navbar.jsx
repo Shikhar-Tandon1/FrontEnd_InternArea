924b0190ddf36c27dd58d192c3af03b1578af9de
Shikhar Tandon <shikhartandon026@gmail.com>
2024-07-27 15:41:20 +0530

    foh commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -226,7 +226,7 @@ Hire Talent
 
  <div className="flex items-center h-9 justify-center mt-4 text-white bg-slate-100 rounded-lg hover:bg-gray-100">
       < Link to={"/phoneauth"}>
-     <button><h4>Login Using Phone</h4></button>  </Link>
+     <button><h4 className='text-gray-500'>Login Using Phone</h4></button>  </Link>
     </div>
 
     <div className="mt-4 flex items-center justify-between">
