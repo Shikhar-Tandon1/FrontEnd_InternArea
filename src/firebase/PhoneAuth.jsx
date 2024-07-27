37b90a4af5d39b2a9eac751d36f3677b3ac54b1f
Shikhar Tandon <shikhartandon026@gmail.com>
2024-07-27 18:12:03 +0530

    fo commit

--- src/firebase/PhoneAuth.jsx
+++ src/firebase/PhoneAuth.jsx
@@ -2,7 +2,35 @@ import { initializeApp } from "firebase/app";
 import { getAuth ,RecaptchaVerifier} from "firebase/auth";
 import { signInWithPhoneNumber } from "firebase/auth";
 import { Link } from 'react-router-dom'
+
 function PhoneAuth(){
+<section class="text-gray-600 body-font relative">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Sign In Using Phone Number</h1>
+    </div>
+    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="phone-number" class="leading-7 text-sm text-gray-600">Phone Number</label>
+            <input type="text" id= "phone-number" placeholder="Enter phone number"/>
+            <br />
+            <button onClick={sendVerificationCode}>Send Verification Code</button>
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="code" class="leading-7 text-sm text-gray-600">Verification Code</label>
+            <input type="text" id="code" placeholder="Enter verification code"/>
+            <button onclick={verifyCode}>Verify OTP</button>
+          </div>
+        </div>
+      </div> 
+    </div>
+  </div>
+</section>
+
 const firebaseConfig = {
   apiKey: "AIzaSyCq7R2C8UnLe4uRsUd4GrUMCd0D_BOQmY0",
   authDomain: "intern-area-1b584.firebaseapp.com",
@@ -46,31 +74,6 @@ const sendVerificationCode=()=>{
             console.log(error)
           })
       }
-      <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Sign In Using Phone Number</h1>
-    </div>
-    <div class="lg:w-1/2 md:w-2/3 mx-auto">
-      <div class="flex flex-wrap -m-2">
-        <div class="p-2 w-1/2">
-          <div class="relative">
-            <label for="phone-number" class="leading-7 text-sm text-gray-600">Phone Number</label>
-            <input type="text" id= "phone-number" placeholder="Enter phone number"/>
-            <br />
-            <button onClick={sendVerificationCode}>Send Verification Code</button>
-          </div>
-        </div>
-        <div class="p-2 w-1/2">
-          <div class="relative">
-            <label for="code" class="leading-7 text-sm text-gray-600">Verification Code</label>
-            <input type="text" id="code" placeholder="Enter verification code"/>
-            <button onclick={verifyCode}>Verify OTP</button>
-          </div>
-        </div>
-      </div> 
-    </div>
-  </div>
-</section>
+      
 }
 export default PhoneAuth
