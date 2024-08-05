04a1951303e816e45efe4c6d0cbc4368f4cebb42
Shikhar Tandon <shikhartandon026@gmail.com>
2024-08-05 11:17:48 +0530

    some commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import DetailApplication from './Applications/DetailApplication';
 import UserApplication from './Profile/UserApplication';
 import UserapplicationDetail from "./Applications/DetailApplicationUser";
 import LanguageSelector from './Components/LanguageSelector';
-import EmailAuth from './firebase/EmailAuth';
 function App() {
   const dispatch=useDispatch();
   useEffect(() => {
@@ -42,7 +41,7 @@ function App() {
     },[dispatch] );
   return (
     <div className="App">
-            <EmailAuth/>
+
             <LanguageSelector/>   
 
 <Navbar/>
